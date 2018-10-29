9c766f2e6e14b75f11d41d5c3f4c405818c400be
tadasny <44547407+tadasny@users.noreply.github.com>
2018-10-29 14:46:18 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,6 @@
+apt update && apt upgrade
+apt install git
+cd ..
+git clone https://github.com/thelinuxchoice/thechoice
+cd Termux-The-Choice
+cp start_thechoice.sh /data/data/com.termux/files/home/
